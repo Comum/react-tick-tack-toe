0bff93f23d3dfc3c6e2a906afa8a6fc242acb294
Comum <miguel.rib.20@gmail.com>
2018-10-12 19:29:12 +0100

    Set up main wrapper

---
+++ src/components/MainWrapper/MainWrapper.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './MainWrapper.css'
+
+const MainWrapper = () => (
+    <div className="mainWrapper">
+        I am the main wrapper
+    </div>
+)
+
+export default MainWrapper
